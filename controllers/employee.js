73d36e4bb511cf746448f72355932bdccf02a814
Alejandro <alexgve7@gmai.com>
2019-12-11 14:54:50 -0600

    employees login

--- controllers/employee.js
+++ controllers/employee.js
@@ -7,10 +7,19 @@ const fs = require('fs')
 controller.logIn = (req,res) => {
     const data = fs.readFileSync(dbWeeding);
     const items = JSON.parse(data)
-    const result  = items.users.find(users => users.id === 9)
-    res.send({
-        data:result
-    })
+    const result  = items.users.find(users => users.id === parseInt(req.body))
+    if(result){
+        res.send({
+            code:200,
+            data:result
+        })
+    }
+    else{
+        res.send({
+            code:404,
+            data:'Not found employee'
+        })
+    }
 }
 
 module.exports = controller
