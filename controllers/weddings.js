8e71d6bab20796903a97614cd130d453db2d19a3
Alejandro <alexgve7@gmai.com>
2019-12-26 16:33:03 -0600

    product add to wedding

--- controllers/weddings.js
+++ controllers/weddings.js
@@ -91,7 +91,7 @@ controller.products = async (req,res) =>{
 }
 controller.addProduct = async (req, res) =>{
     try {
-
+        console.log(req.body)
         var ref = await db.collection('bodas').doc(req.params.id).collection('products');
 
         const result = await ref.doc(req.params.code).set({
